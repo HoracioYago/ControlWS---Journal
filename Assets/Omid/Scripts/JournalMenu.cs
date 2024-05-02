149abab9ffa00308b84e377e51d1596a7f9e830d
HoracioYago <mathias.lust@artfx.fr>
2024-05-02 17:06:49 +0200

    [Add] Final Code

--- Assets/Omid/Scripts/JournalMenu.cs
+++ Assets/Omid/Scripts/JournalMenu.cs
@@ -1,27 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class JournalMenu : MonoBehaviour
 {
     public GameObject[] pages;
     private int currentPageIndex = 0;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            TurnPage(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            TurnPage(-1);
-        }
-    }
+    public GameObject[] choices;
+    private int currentChoiceIndex = 0;
+    public GameObject selectIcon;
 
-    void TurnPage(int direction)
+    public void TurnPage(InputAction.CallbackContext context)
     {
+        if(context.performed) return;
         pages[currentPageIndex].SetActive(false);
-
-        currentPageIndex += direction;
+        float direction = context.ReadValue<Vector2>().x;
+        //float direction = Mathf.Sign(Input.GetAxis("Horizontal"))*1;
+        currentPageIndex += (int)direction;
         if (currentPageIndex >= pages.Length)
         {
             currentPageIndex = 0; 
@@ -30,6 +25,42 @@ public class JournalMenu : MonoBehaviour
         {
             currentPageIndex = pages.Length - 1;
         }
+
+        pages[currentPageIndex].SetActive(true);
+    }
+
+    public void ScrollIndex(InputAction.CallbackContext context)
+    {
+
+            float direction = Mathf.Sign(Input.GetAxis("Vertical")) * 1;
+            currentChoiceIndex += (int)direction;
+            if (currentChoiceIndex >= choices.Length)
+            {
+                currentChoiceIndex = 0;
+            }
+            else if (currentChoiceIndex < 0)
+            {
+                currentChoiceIndex = choices.Length - 1;
+            }
+
+            selectIcon.transform.position = choices[currentChoiceIndex].transform.position;
+        
+    }
+
+    public void SelectPage(InputAction.CallbackContext context)
+    {
+        pages[currentPageIndex].SetActive(false);
+
+        currentPageIndex = currentChoiceIndex + 1;
+
+        pages[currentPageIndex].SetActive(true);
+    }
+
+    public void BackToIndex(InputAction.CallbackContext context)
+    {
+        pages[currentPageIndex].SetActive(false);
+
+        currentPageIndex = 0;
 
         pages[currentPageIndex].SetActive(true);
     }
